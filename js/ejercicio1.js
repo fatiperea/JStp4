c684451ad0ada9b362882d00aca783e603de4d9f
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-15 15:38:24 -0300

    agrego propedad y metodo al objeto y pruebo la salida ya q en el primer intento no se mostraba nada, por intentar usar auto como costructor

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,20 +1,31 @@
-const auto={
-    color:['rojo','negro','azul','blanco','gris'],
-    marca:['renault','chevrolet','fiat','peugeot','ford'], 
-    modelo: 'classic',
-    antiguedad: 2000,
-    patente:'XI0PM',
+const auto = {
+  color: ["rojo", "negro", "azul", "blanco", "gris"],
+  marca: ["renault", "chevrolet", "fiat", "peugeot", "ford"],
+  modelo: "classic",
+  antigüedad: 2000,
+  precio: 3000000,
+  patente: "XI0PM",
 
-    apagar: function(){return 'auto encendido'},
-    encender: function(){return 'el auto se apago'},
+  apagar: function () {
+    document.write("el auto se apago");
+  },
+  encender: function () {
+    return "auto encendido";
+  },
 
-}
+  elegirAuto: function (opcion) {
+    return auto.marca[opcion] + " " + auto.color[opcion];
+  },
+};
+console.log(auto);
+//document.write(auto.encender());
+let prender = auto.encender();
 
-let auto1= new auto();
-let prender=auto1.encender();
-document.write("hola");
+let opcion = parseInt(prompt("Con que auto sales hoy? (1 al 4)"));
 
-document.write(auto1);
+if (opcion <= auto.marca.length) {
+  document.write("Tu elección es: ", auto.elegirAuto(opcion) + "<br>");
+  document.write(prender + "<br>");
+} else document.write("Opción incorrecta, no tenes " + opcion + " autos<br>");
 
-document.write(prender);
-document.write(auto1.apagar());
+auto.apagar();
