6075205c8895bdac2b866241af83d0ef1774f341
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-16 13:05:52 -0300

    agregro atributo y metodo a la clase persona y pruebo la salida

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,9 +1,9 @@
 class Persona{
-    #dni;
-    constructor(nombre,edad,dni/*,sexo,peso,altura,anioNacimiento*/){
+   // #dni;
+    constructor(nombre,edad/*,dni,sexo,peso,altura,anioNacimiento*/){
         this.nombre=nombre;
         this.edad=edad;
-        this.#dni=dni;
+        //this.#dni=dni;
         /*this.sexo=sexo;
         this.peso=peso;
         this.altura=altura;
@@ -13,7 +13,7 @@ class Persona{
 
     get verEdad(){return this.edad}
 
-    get verDNI(){return this.#dni}
+   // get verDNI(){return this.#dni}
 
    /* get verSexo(){return this.sexo}
 
@@ -27,7 +27,7 @@ class Persona{
 
     set darEdad(edad){ this.edad=edad;}
 
-    set darDNI(dni){this.#dni=dni;}
+    //set darDNI(dni){this.#dni=dni;}
 
     /*set darSexo(sexo){this.sexo=sexo;}
 
@@ -35,18 +35,47 @@ class Persona{
 
     set darAlt(altura){this.altura=altura;}*/
 
+    mostrarDatos(){
+        document.write("Nombre: "+this.verNombre+"</br>"+"Edad: "+this.verEdad+"</br>");
+    }
+
 }
 
 class Person extends Persona{
-    constructor(nombre,edad,dni,profesion){
-        super(nombre,edad,dni);
+    constructor(nombre,edad,profesion){
+        super(nombre,edad);
         this.profesion=profesion;
     }
 
-    get profesion(){ return this.profesion;}
+    get verProfesion(){ return this.profesion;}
     set cargaProfesion(profesion){this.profesion=profesion;}
 
-    saludar(){document.write(this.verNombre+" dice buen dia");}
+    saludar(){document.write(this.verNombre+" dice buen dia</br>");}
 
-    despedirse(){document.write(this.verNombre+" dice adios");}
+    despedirse(){document.write(this.verNombre+" dice adios</br>");}
+
+    mostrarDatos(){
+        document.write("Nombre: "+this.verNombre+"</br>"+"Edad: "+this.verEdad+"</br>"+"Profesion: "+this.verProfesion+"</br>");
+    }
 }
+const nombre1=prompt("Ingrese nombre de la 1° persona: ");
+const edad1=prompt("Ingrese edad: ");
+const profesion1=prompt("Ingrese profesion: ");
+const individuo= new Person(nombre1,edad1,profesion1);
+
+console.log(individuo);
+
+const nombre2=prompt("Ingrese nombre de la 2° persona: ");
+const edad2=prompt("Ingrese edad: ");
+const profesion2=prompt("Ingrese profesion: ");
+const individuo1= new Person(nombre2,edad2,profesion2);
+
+console.log(individuo1);
+
+individuo.mostrarDatos();
+individuo.saludar();
+individuo1.mostrarDatos();
+individuo1.saludar();
+
+individuo.despedirse();
+individuo1.despedirse();
