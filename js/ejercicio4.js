a2dda271048d7e7a8eb43200b7fedca1cc69e19a
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-20 22:21:47 -0300

    corrijo getters y setters y el metodo para imprimir

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,48 +1,48 @@
 class Producto {
   constructor(codigo, nombre, precio) {
-    //this.codigo = codigo;
+    this.codigo = codigo;
     this.nombre = nombre;
     this.precio = precio;
   }
-  set codigo(nuevoCodigo) {
+  set altaCodigo(nuevoCodigo) {
     this.codigo = nuevoCodigo;
   }
- /* set nombre(nuevoNombre) {
+  set altaNombre(nuevoNombre) {
     this.nombre = nuevoNombre;
   }
 
-  set precio(nuevoPrecio) {
-    this.precio = nuevoPrecio;
-  }*/
-  get codigo() {
+  set altaPrecio(nuevoPrecio) {
+    if(nuevoPrecio>0)
+      this.precio = nuevoPrecio;
+  }
+  get verCodigo() {
     return this.codigo;
   }
-/*
-  get nombre() {
+
+  get verNombre() {
     return this.nombre;
   }
 
-  get precio() {
+  get verPrecio() {
     return this.precio;
-  }*/
+  }
 
-  imprimeDatos() {
-    document.write(
-      "Codigo: " +
-        this.codigo 
-       /* "</br>" +
-        "Nombre: " +
-        this.nombre +
-        "</br>" +
-        "Precio: $" +
-        this.precio*/
-    );
+ imprimeDatos() {
+    document.write(this.verCodigo + " " +this.verNombre +" " + this.verPrecio+"</br>");
   }
 }
 
 //console.log(producto);
 
 const producto1 = new Producto(1111, "fcvb001", 3550);
-producto1.imprimeDatos();
-const arrayProductos = [];
-//producto.imprimeDatos();
+const producto2 = new Producto(1112, "fcvb002", 2500);
+const producto3 = new Producto(1113, "fcvb003", 1550);
+
+//producto1.imprimeDatos();
+const arrayProductos = [producto1,producto2,producto3];
+//document.write(arrayProductos);
+document.write("Codigo:  Nombre: Precio:</br>"); 
+for(let i=0;i<arrayProductos.length;i++)
+{
+  arrayProductos[i].imprimeDatos();
+}
